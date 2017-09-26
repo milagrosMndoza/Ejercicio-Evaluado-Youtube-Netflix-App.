778313e10a5addd7ea9b167772c4d14fa5c234ed
milagrosMndoza <milagrosmndoza@gmail.com>
2017-09-26 10:17:09 -0500

    Subiendo archivos

---
+++ app.js
@@ -0,0 +1,55 @@
+"use strict";
+const API_KEY "AIzaSyC3beUrK0-c49t50ztheqHzKtPYw949C1c";
+class video {
+	constructor() {
+		this.result {}
+		videos: [], selectedVideo: null, searchTerm: "iPhone X"
+	},
+}
+//<iframe className="embed-responsive-item" src={url}> </iframe>
+getVideoList(videos) {
+	return videos.map((video, index) => {
+		const imageUrl = video.snippet.thumbnails.default.url;
+		const url = `https://www.youtube.com/embed/${video.id.videoId}`;
+		return `<li> 
+                     <img class="media-object" src=${imageUrl} /> 
+                     <p> 
+                        <iframe class="embed-responsive-item" src=${url}> </iframe>
+                     </p>
+               </li>`;
+	});
+}, youtubeSearch(searchTerm) {
+	console.log(searchTerm);
+	YTSearch({
+		key: API_KEY,
+		term: searchTerm
+	}, data => {
+		console.log("result", data);
+		app.result = {
+			videos: data,
+			selectedVideo: data[0],
+			searchTerm: searchTerm
+		};
+		var list = app.getVideoList(app.result.videos);
+		console.log("lis: ", list);
+		$("#root").append(list);
+	});
+}, videoSearch(searchTerm) {
+	jQuery.getJSON("list.json", data => {
+		console.log("result", data.items);
+		app.result = {
+			videos: data.items,
+			selectedVideo: data.items[0],
+			searchTerm: searchTerm
+		};
+		var list = app.getVideoList(app.result.videos);
+		console.log("lis: ", list);
+		$("#root").append(list);
+	});
+}
+};
+$(document).ready(app.init);
+init() {
+	//app.videoSearch("iPhone");
+	app.youtubeSearch("iPhone X");
+},
